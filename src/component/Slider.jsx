4a562feb487502f1ba00fda6d84e1a08a158182e
Goldyn19 <nwaezegolden2002@gmail.com>
2024-07-08 21:34:23 +0100

    changes to carousel

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Carousel from "react-elastic-carousel";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import CollectionCard from "./CollectionCard";
 import pic1 from '../assets/images/collection1.png';
 import pic2 from '../assets/images/collection2.png';
@@ -8,32 +9,57 @@ import pic4 from '../assets/images/collection4.png';
 import pic5 from '../assets/images/collection5.png';
 
 const Slider = ({ slides, deviceType }) => {
-  const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2 },
-    { width: 768, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 4 },
-  ];
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
   return (
     <>
       <h1 style={{ textAlign: "center" }}>Example to setup your carousel in react</h1>
       <div className="App">
         <Carousel 
-          breakPoints={breakPoints}
-          showArrows={false}
-          showEmptySlots={false}
+          responsive={responsive}
+          arrows={false}
+          showDots={false}
+          infinite={true}
+          autoPlay={true}
+          autoPlaySpeed={3000}
+          keyBoardControl={true}
+          customTransition="all .5"
+          transitionDuration={500}
+          containerClass="carousel-container"
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          deviceType={deviceType}
+          itemClass="carousel-item-padding"
         >
-           <CollectionCard imgSrc={pic1} name={'Arc Duke Flur'} />
-                <CollectionCard imgSrc={pic2} name={'Fluffy Accent'} />
-                <CollectionCard imgSrc={pic3} name={'Emily'} />
-                <CollectionCard imgSrc={pic4} name={'Dracular Horn'} />
-                <CollectionCard imgSrc={pic5} name={'Antique Barrel'} />
+          <CollectionCard imgSrc={pic1} name={'Arc Duke Flur'} />
+          <CollectionCard imgSrc={pic2} name={'Fluffy Accent'} />
+          <CollectionCard imgSrc={pic3} name={'Emily'} />
+          <CollectionCard imgSrc={pic4} name={'Dracular Horn'} />
+          <CollectionCard imgSrc={pic5} name={'Antique Barrel'} />
         </Carousel>
       </div>
       <style jsx>{`
-        .rec-dot {
+        .react-multi-carousel-dot-list {
           display: none;
+        }
+        .carousel-item-padding {
+          margin: 0 15px; /* Adjust the padding to your desired space */
         }
       `}</style>
     </>
